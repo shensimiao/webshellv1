d471245d6c61d1480935dad0ca60f9ff8821a18a
shensi <z1137276805@foxmail.com>
2023-02-23 10:48:58 +0800

    2.20.10.55

--- webshell/views.py
+++ webshell/views.py
@@ -185,7 +185,7 @@ def opaiai(request):
     data = json.loads(request.body.decode('utf-8'))
     context = {}
     # try:
-    openai.api_key = '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'
+    openai.api_key = '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'
     prompt = data['reason']
     model = "text-davinci-003"
     temperature = 0.5
